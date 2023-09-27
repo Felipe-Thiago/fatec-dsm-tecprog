e20b3d819b4049be883c9370540e75b5fbd952ad
Felipe Thiago <felipethiagodasilva@gmail.com>
2023-09-26 22:14:26 -0300

    mudança recursao

--- EstruturaDeDados/src/recursao/Fatorial.java
+++ EstruturaDeDados/src/recursao/Fatorial.java
@@ -1,28 +1,30 @@
-
-import recursao.Fatorial;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package recursao;
 
 /**
  *
  * @author Alunos
  */
-public class EstruturaDeDados {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-       Fatorial fatorial = new Fatorial();
-       int num = 4;
-       
-       num = fatorial.fatorialRec(num);
-        System.out.println(num);
+public class Fatorial {
+    public int fatorialRec(int num){
+        if (num == 0){
+            return 1;
+        }
+        
+        num = num * fatorialRec(num - 1);
+        return num;
     }
     
+    public static void main(String[] args) {
+       Fatorial fatorial = new Fatorial();
+       int num = 6;
+       int num2 = num;
+       
+       num = fatorial.fatorialRec(num);
+        System.out.println("O fatorial de " + num2 + " é " + num);
+    }
 }
